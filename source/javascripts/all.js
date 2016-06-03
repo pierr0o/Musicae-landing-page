41419878cad2609d82efbf363a9036f2699473a1
Pierre <pierrepariente@Pierres-MacBook-Pro-2.local>
2016-06-03 01:38:40 -0700

    Landing page

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,20 +2,19 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
-$(document).ready(function() {
+// $(document).ready(function() {
    
-  $(window).scroll(function() {
-    var heightTop = $(window).scrollTop();
-    if (heightTop  > 100) {
-    	$('#nav-fix').addClass("navbar-fixed-top");
-    }
-    else {
-    	$('#nav-fix').removeClass("navbar-fixed-top");
-    }
-  });
+//   $(window).scroll(function() {
+//     var heightTop = $(window).scrollTop();
+//     if (heightTop  > 100) {
+//     	$('#nav-fix').addClass("navbar-fixed-top").css("opacity", "0.6");
+//     }
+//     else {
+//     	$('#nav-fix').removeClass("navbar-fixed-top").css("opacity", "");
+//     }
+//   });
 
-  $('.single-item').slick();
   
-});
+// });
 
 
