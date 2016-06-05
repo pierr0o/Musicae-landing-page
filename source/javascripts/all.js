24c0f3a094edad7b69808320e5e4e69a20f304a2
Pierre <pierrepariente@Pierres-MacBook-Pro-2.local>
2016-06-04 18:11:24 -0700

    slick integration

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,19 +2,14 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
-// $(document).ready(function() {
-   
-//   $(window).scroll(function() {
-//     var heightTop = $(window).scrollTop();
-//     if (heightTop  > 100) {
-//     	$('#nav-fix').addClass("navbar-fixed-top").css("opacity", "0.6");
-//     }
-//     else {
-//     	$('#nav-fix').removeClass("navbar-fixed-top").css("opacity", "");
-//     }
-//   });
+$(document).ready(function(){
+	$('.slider').slick({
+		centerMode: true,
+		slidesToShow: 3,
+		centerPadding: '6px',
+	});
+	  
+});
 
-  
-// });
 
 
