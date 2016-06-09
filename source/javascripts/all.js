f7d32f42c5f176634124f4832471bd21db5943a0
Pierre <pierrepariente@Pierres-MacBook-Pro-2.local>
2016-06-09 00:22:12 -0700

    slick spotify

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -4,9 +4,24 @@
 
 $(document).ready(function(){
 	$('.slider').slick({
+		dots: true,
+		speed: 300,
 		centerMode: true,
-		slidesToShow: 3,
+		slidesToShow: 2,
 		centerPadding: '6px',
+
+	});
+
+	$(window).scroll(function() {    
+	    var scroll = $(window).scrollTop();
+	    var textColorScroll = $(".navbar-nav li a").css("color", "white");
+	    if (scroll >= 76) {
+	    	$(".navbar").addClass("navDark");
+	        $(".navbar-nav li a").addClass("textColorScroll");
+	    }
+	    if (scroll <= 76) {
+	        $(".navbar").removeClass("navDark");
+	    }
 	});
 	  
 });
